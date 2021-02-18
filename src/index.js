857f5fde9056e5fe5437fa294bb6bed31b8289b6
Zhaoxian Hu <huzhx@uci.edu>
2021-02-17 23:39:51 -0800

    implement a simple apollo server with a defined graphql schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import 'dotenv/config';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log('Hello ever running Node.js project.');
+const app = express();
 
-console.log(process.env.MY_SECRET);
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch',
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
